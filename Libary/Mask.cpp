14b297bbce106308ed576f89832a44c0f4d6d683
jeroenhuisen <jeroen.huisen@student.hu.nl>
2014-04-01 11:59:22 +0200

    Added some comments

--- Libary/Mask.cpp
+++ Libary/Mask.cpp
@@ -2,8 +2,8 @@
 
 Mask::Mask(Image img, int s){
 	size = s;
-	mask = new unsigned char[s*s];
 	image = img;
+	mask = new unsigned char[s*s];
 }
 
 Mask::~Mask(){
@@ -11,9 +11,9 @@ Mask::~Mask(){
 }
 
 unsigned char * Mask::getMask(int x, int y, int channel){
-	int filterNumber = 0;
-	for (int filterY = -(size - 1) / 2; filterY <= (size - 1) / 2; filterY++){
-		for (int filterX = -(size - 1) / 2; filterX <= (size - 1) / 2; filterX++){
+	int filterNumber = 0;//used for saving the mask from left to right in mask.
+	for (int filterY = -(size - 1) / 2; filterY <= (size - 1) / 2; filterY++){//Controls y starts from the top of the mask ends at the bottom
+		for (int filterX = -(size - 1) / 2; filterX <= (size - 1) / 2; filterX++){//Controls x starts from the left of the mask ends at the right
 			mask[filterNumber] = *image.Data(x + filterX, y + filterY, channel);
 			filterNumber++;
 		}
--- Libary/Mask.h
+++ Libary/Mask.h
@@ -6,7 +6,23 @@ private:
 	int size;
 	unsigned char * mask;
 public:
+	//Constructor Mask
+	//
+	//Saves s, the size of the mask
+	//Img, the image of which the mask should be applied too.
+	//Also the mask pointer is allocated.
 	Mask(Image img, int s);
+
+	//Destructor Mask
+	//
+	//Deletes allocated mask pointer
 	~Mask();
+
+	//Returns the mask
+	//
+	//The function will return the mask and calculate the values according to x,y and channel.
+	//X and y are the x and y coordinates of the mid pixel.
+	//Channel is the channel of the image which the mask should be applied.
+	//It returns mask, which contains the mask from topleft to rightbottom.
 	unsigned char * getMask(int x, int y, int channel);
 };
