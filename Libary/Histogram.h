2379d1a574f7cafcb78c2a74961dc5a6bbc9bc8d
jeroenhuisen <jeroen.huisen@student.hu.nl>
2014-04-06 23:49:10 +0200

    come bad comments

--- Libary/Histogram.h
+++ Libary/Histogram.h
@@ -14,10 +14,15 @@ private:
 	int total;
 
 public:
+	//Histogram takes param on which image histogram should be applied too
 	Histogram(Image image);
+	//Makes the histogram choose value for range and channel for which channel of image should make a histogram
 	void MakeAHistogram(int value, int channel);
+	//returns histogram
 	int * getHistogram();
+	//returns total
 	int getTotal();
+	//saves the histogram with filenam
 	void SaveHistogram(const char * filename);
 };
 #endif
